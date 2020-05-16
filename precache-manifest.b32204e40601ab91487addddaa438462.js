4b48503662cd3a12749989d122d9ec74ddd4c926
Danny Yang <dannyyang@Dannys-MacBook-Pro.local>
2020-05-16 14:42:04 -0700

    Updates

---
+++ precache-manifest.b32204e40601ab91487addddaa438462.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13eef41fc1ed5ea7a52f2a656c50f72a",
+    "url": "/ducks/index.html"
+  },
+  {
+    "revision": "b09eb4e476d9d8564cf9",
+    "url": "/ducks/static/css/main.60135cee.chunk.css"
+  },
+  {
+    "revision": "d1ca1311e07d0ffe6b56",
+    "url": "/ducks/static/js/2.43c113dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ducks/static/js/2.43c113dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09eb4e476d9d8564cf9",
+    "url": "/ducks/static/js/main.c54f14ed.chunk.js"
+  },
+  {
+    "revision": "3124b81b9e93abf90aef",
+    "url": "/ducks/static/js/runtime-main.b1bdac7d.js"
+  },
+  {
+    "revision": "75e3a72ad63a1cec47497990ff0d97f1",
+    "url": "/ducks/static/media/peeking.75e3a72a.gif"
+  },
+  {
+    "revision": "e533d6e280ad7bc66073146a2b9336f1",
+    "url": "/ducks/static/media/welcome.e533d6e2.gif"
+  }
+]);
